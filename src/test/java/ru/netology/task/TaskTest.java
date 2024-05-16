2cc5aeefccd1795dbfce0441ca8c6bee0071074b
NadezhdaT <kybalibri@mail.ru>
2024-05-16 21:53:44 +0700

    initial commit

---
+++ src/test/java/ru/netology/task/TaskTest.java
@@ -0,0 +1,86 @@
+package ru.netology.task;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+    @Test
+    public void testMatchSimpleTask() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        boolean actual = simpleTask.matches("Позвонить");
+
+        Assertions.assertTrue(actual);
+
+    }
+
+    @Test
+    public void testNotMatchSimpleTask() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        boolean actual = simpleTask.matches("Написать");
+
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    public void testMatchEpic() {
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        boolean actual = epic.matches("Хлеб");
+
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    public void testNotMatchEpic() {
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        boolean actual = epic.matches("Сыр");
+
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    public void testMatchMeetingByTopic() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда");
+
+        boolean actual = meeting.matches("приложения");
+
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    public void testMatchMeetingByProject() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда");
+
+        boolean actual = meeting.matches("Приложение");
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    public void testNotMatchMeeting() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда");
+
+        boolean actual = meeting.matches("вторник");
+        Assertions.assertFalse(actual);
+    }
+
+
+}
